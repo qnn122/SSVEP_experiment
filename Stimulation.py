d8f08372390260ca8e04e5bf138fffbba5788012
quangNguyen122 <bme.nnquang@gmail.com>
2016-02-20 23:44:41 +0700

    Reorganize the project
    Make class out of craft codes.
    Create folders images which stores arrow photos.

--- Stimulation.py
+++ Stimulation.py
@@ -12,112 +12,139 @@ from VisionEgg.Text import Text
 from VisionEgg.Textures import *
 import time
 
+class Stimulation:
+    global TopRate, BotRate, LeftRate, RightRate
+    # Set up stimulus's frequency
+    TopRate =   5              # Hz
+    BotRate = 10
+    LeftRate = 15
+    RightRate = 20
 
-# Set up stimulus's frequency
-TopRate =   5              # Hz
-BotRate = 10
-LeftRate = 15
-RightRate = 20
+    def __init__(self):
+        self._running = True
 
-# Setting screen
-screen = get_default_screen()
-screen.set( bgcolor= (0., 0., 0.) ) # black
-w = screen.size[0]
-h = screen.size[1]
+        # Initialize screen
+        self.screen = get_default_screen()
 
-HSize = 100
-VSize = 50
-# Target stimulus
-TopTarget = Target2D(size= (HSize, VSize),
-                     color= (1.0, 1.0, 1.0, 1.0),
-                     position=(w/2, h*0.8))
+        # Get dimension of the screen
+        w = self.screen.size[0]
+        h = self.screen.size[1]
 
-BotTarget = Target2D(size= (HSize, VSize),
-                     color= (1.0, 1.0, 1.0, 1.0),
-                     position=(w/2, h*0.2))
+        # Set dimension of the targets
+        HSize = 100
+        VSize = 50
 
-LeftTarget = Target2D(size= (VSize, HSize),
-                      color= (1.0, 1.0, 1.0, 1.0),
-                      position=(w*0.2, h/2))
+        # Number of trials
+        self.numTrial = 3
 
-RightTarget = Target2D(size= (VSize, HSize),
-                       color= (1.0, 1.0, 1.0, 1.0),
-                       position=(w*0.8, h/2))
-# Message
-text = Text(text='Please wait for next trial...',
-            color=(1.0, 0.5, 0.5),
-            position=(w/2, h*0.8),
-            font_size=50,
-            anchor='center',
-            on=False)
+        # Initialize Targets
+        self.TopTarget = Target2D(size= (HSize, VSize),
+                                  color= (1.0, 1.0, 1.0, 1.0),
+                                  position=(w/2, h*0.8))
 
-# Direction
-filename = "left.bmp"
-texture = Texture(filename)
-arrow = TextureStimulus(texture=texture,
-                        position=(w/2, h/2),
-                        anchor='center',
-                        on=False)
+        self. BotTarget = Target2D(size= (HSize, VSize),
+                                   color= (1.0, 1.0, 1.0, 1.0),
+                                   position=(w/2, h*0.2))
 
-viewport1 = Viewport(screen=screen,
-                     stimuli=[TopTarget, BotTarget, LeftTarget, RightTarget])
+        self.LeftTarget = Target2D(size= (VSize, HSize),
+                                   color= (1.0, 1.0, 1.0, 1.0),
+                                   position=(w*0.2, h/2))
 
-viewport2 = Viewport(screen=screen,
-                     stimuli=[text, arrow])
+        self.RightTarget = Target2D(size= (VSize, HSize),
+                                    color= (1.0, 1.0, 1.0, 1.0),
+                                    position=(w*0.8, h/2))
 
-p = Presentation(go_duration=(3,'seconds'),viewports=[viewport1])
+        # Message
+        self.text = Text(text='Please wait for next trial...',
+                         color=(1.0, 0.5, 0.5),
+                         position=(w/2, h*0.8),
+                         font_size=50,
+                         anchor='center',
+                         on=False)
 
-p2 = Presentation(go_duration=(2,'seconds'), viewports=[viewport2])
+        # Arrows
+        self.arrow = TextureStimulus(texture=Texture('images\left2.bmp'),
+                                     position=(w/2, h/2),
+                                     anchor='center',
+                                     on=False)
 
-# Open file for writing data
-file = open("Recordingfile.txt", "w")
+        # Viewports to stick graphical objects to screen
+        self.viewport1 = Viewport(screen=self.screen,
+                                  stimuli=[self.TopTarget, self.BotTarget, self.LeftTarget, self.RightTarget])
 
-# Flickering method
-def TopFlick(t):
-    return int(t*TopRate*2.0) % 2
+        self.viewport2 = Viewport(screen=self.screen,
+                                  stimuli=[self.text, self.arrow])
 
-def BotFlick(t):
-    return int(t*BotRate*2.0) % 2
+        # Presentations (for controlling timing)
+        self.p = Presentation(go_duration=(3, 'seconds'), viewports=[self.viewport1])
 
-def LeftFlick(t):
-    return int(t*LeftRate*2.0) % 2
+        self.p2 = Presentation(go_duration=(2, 'seconds'), viewports=[self.viewport2])
 
-def RightFlick(t):
-    return int(t*RightRate*2.0) % 2
+    def on_init(self):
+        # Set screen's background color
+        self.screen.set(bgcolor=(0., 0., 0.)) # black
 
-def appear():
-    return True
+        # Set control's parameters and corresponding function. Controlling targers
+        self.p.add_controller(self.TopTarget, 'on',     FunctionController(during_go_func=self.topFlick))
+        self.p.add_controller(self.BotTarget, 'on',     FunctionController(during_go_func=self.botFlick))
+        self.p.add_controller(self.LeftTarget, 'on',    FunctionController(during_go_func=self.leftFlick))
+        self.p.add_controller(self.RightTarget, 'on',   FunctionController(during_go_func=self.rightFlick))
 
-def appear_t(t):
-    flag = True
-    if flag:
-        writedata(t, 1)
-        flag = False
+        # Controlling others
+        # self.p2.add_controller(self.text, 'on',         FunctionController(during_go_func=self.appear))
+        self.p2.add_controller(self.arrow, 'on',        FunctionController(during_go_func=self.arrow_appear))
 
-    if p.is_in_go_loop():
-        return False
-    else:
-        return True
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
 
-def disapp(t):
-    return False
+        for i in range(0,self.numTrial):
+            self.p.go()
+            self.p2.go()
 
-def writedata(time, target_flag):
-    line = str(time) + '\t' + str(target_flag) + '\n'
-    file.write(line)
+    #################################################
+    # Controlling methods: Flickering               #
+    #################################################
+    def topFlick(self,t):
+        return int(t*TopRate*2.0) % 2
 
-p.add_controller(TopTarget,'on', FunctionController(during_go_func=TopFlick))
-p.add_controller(BotTarget,'on', FunctionController(during_go_func=BotFlick))
-p.add_controller(LeftTarget,'on', FunctionController(during_go_func=LeftFlick))
-p.add_controller(RightTarget,'on', FunctionController(during_go_func=RightFlick))
-p2.add_controller(text, 'on', FunctionController(during_go_func=appear_t))
-p2.add_controller(arrow, 'on', FunctionController(during_go_func=appear_t))
+    def botFlick(self,t):
+        return int(t*BotRate*2.0) % 2
 
+    def leftFlick(self,t):
+        return int(t*LeftRate*2.0) % 2
 
-numTrial = 3
+    def rightFlick(self,t):
+        return int(t*RightRate*2.0) % 2
 
-for i in range(0,numTrial):
-    p.go()
-    p2.go()
+    def arrow_appear(self, t):
+        flag = True
+        if flag:
+            #self.writedata(t, 1)
+            flag = False
 
-file.close()
+        if self.p.is_in_go_loop():
+            return False
+        else:
+            return True
+
+    #################################################
+    # Writing data methods                          #
+    #################################################
+    def on_writing(self):
+        # Open file for writing data
+        self.file = open("Recordingfile.txt", "w")
+
+    def on_close(self):
+        self.file.close()
+
+    def writedata(self, time, target_flag):
+        line = str(time) + '\t' + str(target_flag) + '\n'
+        file.write(line)
+
+"""
+    Main
+"""
+if __name__ == "__main__":
+    exp = Stimulation()
+    exp.on_execute()
