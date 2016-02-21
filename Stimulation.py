7739acc2dfdfce3ae2a35a236deb8de2df9393e9
quangNguyen122 <bme.nnquang@gmail.com>
2016-02-21 19:35:49 +0700

    Add writing process and initializing process

--- Stimulation.py
+++ Stimulation.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
-"""Display 4
+"""Display 4 targets with cues before each
+
+The position code of 4 targets
++++++++++++ 1 ++++++++++
++                       +
+2                       3
++                       +
++++++++++++ 4 +++++++++++
 """
 
 from VisionEgg import *
+# TODO
+# Skip the setting screen, jump directly to the main program
+# TODO
+# Turn off App Log
 start_default_logging(); watch_exceptions()
 
 from VisionEgg.Core import *
@@ -15,7 +26,7 @@ import time
 class Stimulation:
     global TopRate, BotRate, LeftRate, RightRate, HSize, VSize
     global numTrial, order_appear
-    global duration_cue, duration_target, duration_trial, total_time
+    global duration_init, duration_cue, duration_target, duration_trial, total_time
     # Set up stimulus's frequency
     TopRate =   5              # Hz
     BotRate = 10
@@ -27,18 +38,21 @@ class Stimulation:
     VSize = 50
 
     # Experiment parameters
-    numTrial = 10
+    numTrial = 4
     order_appear = np.uint16(4*np.random.random(numTrial)+1)
+    duration_init = 2
     duration_target = 3
     duration_cue = 2
     duration_trial = duration_target + duration_cue
-    total_time = duration_trial*numTrial
+    total_time = duration_init + duration_trial*numTrial
 
     def __init__(self):
         global w, h
         self._running = True
 
         # Initialize screen
+        # TODO
+        # Set fullscreen
         self.screen = get_default_screen()
 
         # Get dimension of the screen
@@ -52,12 +66,12 @@ class Stimulation:
         self.RightTarget = Target2D()
 
         # Message
-        self.text = Text(text='Please wait for next trial...',
+        self.mess = Text(text='Please wait for next trial...',
                          color=(1.0, 0.5, 0.5),
                          position=(w/2, h*0.8),
                          font_size=50,
                          anchor='center',
-                         on=False)
+                         on=True)
 
         # Arrows
         self.arrow = TextureStimulus()
@@ -66,11 +80,17 @@ class Stimulation:
         self.viewport1 = Viewport(screen=self.screen,
                                   stimuli=[self.TopTarget, self.BotTarget, self.LeftTarget, self.RightTarget])      # For Targets
         self.viewport2 = Viewport(screen=self.screen,
-                                  stimuli=[self.text, self.arrow])                                                  # For cue (and other things)
+                                  stimuli=[self.arrow])                                                  # For cue (and other things)
+        self.viewport3 = Viewport(screen=self.screen,
+                                  stimuli=[self.mess])                                                  # For cue (and other things)
 
         # Presentations (for controlling timing)
-        self.p = Presentation(go_duration=(duration_target, 'seconds'), viewports=[self.viewport1])
-        self.p2 = Presentation(go_duration=(duration_cue, 'seconds'), viewports=[self.viewport2])
+        self.initialize = Presentation(go_duration=(duration_init, 'seconds'), viewports=[self.viewport3])
+        self.targetShow = Presentation(go_duration=(duration_target, 'seconds'), viewports=[self.viewport1])
+        self.cueShow = Presentation(go_duration=(duration_cue, 'seconds'), viewports=[self.viewport2])
+
+        # For result
+        self.TargetLog = np.zeros((numTrial, 2))     # First column: target code,
 
     def on_init(self):
         # Set screen's background color
@@ -104,24 +124,38 @@ class Stimulation:
                         anchor='center',
                         on=False)
 
+        # Show initializing message
+        self.initialize.add_controller(self.mess, 'text',     FunctionController(during_go_func=self.showMess))
+
         # Set control's parameters and corresponding function. Controlling targers
-        self.p.add_controller(self.TopTarget, 'on',     FunctionController(during_go_func=self.topFlick))
-        self.p.add_controller(self.BotTarget, 'on',     FunctionController(during_go_func=self.botFlick))
-        self.p.add_controller(self.LeftTarget, 'on',    FunctionController(during_go_func=self.leftFlick))
-        self.p.add_controller(self.RightTarget, 'on',   FunctionController(during_go_func=self.rightFlick))
+        self.targetShow.add_controller(self.TopTarget, 'on',     FunctionController(during_go_func=self.topFlick))
+        self.targetShow.add_controller(self.BotTarget, 'on',     FunctionController(during_go_func=self.botFlick))
+        self.targetShow.add_controller(self.LeftTarget, 'on',    FunctionController(during_go_func=self.leftFlick))
+        self.targetShow.add_controller(self.RightTarget, 'on',   FunctionController(during_go_func=self.rightFlick))
 
         # Controlling others
         # self.p2.add_controller(self.text, 'on',         FunctionController(during_go_func=self.appear))
-        self.p2.add_controller(self.arrow, 'on',        FunctionController(during_go_func=self.arrow_appear))
-        self.p2.add_controller(self.arrow, 'texture',   FunctionController(during_go_func=self.random_cue))
+        self.cueShow.add_controller(self.arrow, 'on',        FunctionController(during_go_func=self.arrow_appear))
+        self.cueShow.add_controller(self.arrow, 'texture',   FunctionController(during_go_func=self.random_cue))
+
 
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
 
+        # Prompting
+        self.initialize.go()
+
+        # Experiment goes..
         for i in range(0,numTrial):
-            self.p.go()
-            self.p2.go()
+            self.cueShow.go()
+            self.targetShow.go()
+
+        self.on_writing()
+
+    # TODO
+    # Maybe we need a method for closing the program
+
 
     #################################################
     # Controlling methods: Flickering               #
@@ -144,42 +178,50 @@ class Stimulation:
             #self.writedata(t, 1)
             flag = False
 
-        if self.p.is_in_go_loop():
+        if self.targetShow.is_in_go_loop():
             return False
         else:
             return True
 
     def random_cue(self,t):
-        start_time = self.p.last_go_loop_start_time_absolute_sec
-        # if not isinstance(start_time, type(None)):
-        try:    # catch the wrong type of start_time returned earlier
-            i = int(start_time/duration_trial)
-        finally:
-            return Texture('images/left2.bmp')  # Just return something
+        start_time = self.cueShow.last_go_loop_start_time_absolute_sec
+        # TODO
+        # Add try catch to handle errors regarding start_time's type problem
+        # TODO
+        # Add event so that the transition processes only taken place once
 
-        cur_target = order_appear[i]        # current target
-        if cur_target == 1:
-            return Texture('images/left2.bmp')
-        elif cur_target == 2:
-            return Texture('images/right2.bmp')
-        elif cur_target == 3:
-            return Texture('images/up2.bmp')
-        elif cur_target == 4:
-            return Texture('images/down2.bmp')
+        if not isinstance(start_time, type(None)):
+            i = int((start_time-duration_init)/duration_trial)  # eliminate initializing duration factor
+            cur_target = order_appear[i]        # current target
+            self.TargetLog[i, :] = np.array([cur_target, start_time])   # return real time
+            if cur_target == 1:
+                return Texture('images/up2.bmp')
+            elif cur_target == 2:
+                return Texture('images/left2.bmp')
+            elif cur_target == 3:
+                return Texture('images/right2.bmp')
+            elif cur_target == 4:
+                return Texture('images/down2.bmp')
+        else:
+            return Texture('images/left2.bmp')  # Just return something, add_controller need it
+
+    def showMess(self, t):
+        return 'Ready ...'
+
 
     #################################################
     # Writing data methods                          #
     #################################################
     def on_writing(self):
         # Open file for writing data
-        self.file = open("Recordingfile.txt", "w")
+        file = open("Recordingfile.txt", "w")
 
-    def on_close(self):
-        self.file.close()
+        for i in range(self.TargetLog.shape[0]):
+            target = self.TargetLog[i, 0]
+            time = self.TargetLog[i, 1]
+            line = str(target) + '\t' + str(time) + '\n'
+            file.write(line)
 
-    def writedata(self, time, target_flag):
-        line = str(time) + '\t' + str(target_flag) + '\n'
-        file.write(line)
 
 """
     Main
