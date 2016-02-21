90a93adeec54b20ff63b39ae535d58e5e0f0eb76
quangNguyen122 <bme.nnquang@gmail.com>
2016-02-21 18:27:34 +0700

    Fix cue problem

--- Stimulation.py
+++ Stimulation.py
@@ -13,14 +13,29 @@ from VisionEgg.Textures import *
 import time
 
 class Stimulation:
-    global TopRate, BotRate, LeftRate, RightRate
+    global TopRate, BotRate, LeftRate, RightRate, HSize, VSize
+    global numTrial, order_appear
+    global duration_cue, duration_target, duration_trial, total_time
     # Set up stimulus's frequency
     TopRate =   5              # Hz
     BotRate = 10
     LeftRate = 15
     RightRate = 20
 
+    # Set dimension of the targets
+    HSize = 100
+    VSize = 50
+
+    # Experiment parameters
+    numTrial = 10
+    order_appear = np.uint16(4*np.random.random(numTrial)+1)
+    duration_target = 3
+    duration_cue = 2
+    duration_trial = duration_target + duration_cue
+    total_time = duration_trial*numTrial
+
     def __init__(self):
+        global w, h
         self._running = True
 
         # Initialize screen
@@ -30,29 +45,11 @@ class Stimulation:
         w = self.screen.size[0]
         h = self.screen.size[1]
 
-        # Set dimension of the targets
-        HSize = 100
-        VSize = 50
-
-        # Number of trials
-        self.numTrial = 3
-
         # Initialize Targets
-        self.TopTarget = Target2D(size= (HSize, VSize),
-                                  color= (1.0, 1.0, 1.0, 1.0),
-                                  position=(w/2, h*0.8))
-
-        self. BotTarget = Target2D(size= (HSize, VSize),
-                                   color= (1.0, 1.0, 1.0, 1.0),
-                                   position=(w/2, h*0.2))
-
-        self.LeftTarget = Target2D(size= (VSize, HSize),
-                                   color= (1.0, 1.0, 1.0, 1.0),
-                                   position=(w*0.2, h/2))
-
-        self.RightTarget = Target2D(size= (VSize, HSize),
-                                    color= (1.0, 1.0, 1.0, 1.0),
-                                    position=(w*0.8, h/2))
+        self.TopTarget = Target2D()
+        self.BotTarget = Target2D()
+        self.LeftTarget = Target2D()
+        self.RightTarget = Target2D()
 
         # Message
         self.text = Text(text='Please wait for next trial...',
@@ -63,26 +60,49 @@ class Stimulation:
                          on=False)
 
         # Arrows
-        self.arrow = TextureStimulus(texture=Texture('images\left2.bmp'),
-                                     position=(w/2, h/2),
-                                     anchor='center',
-                                     on=False)
+        self.arrow = TextureStimulus()
 
         # Viewports to stick graphical objects to screen
         self.viewport1 = Viewport(screen=self.screen,
-                                  stimuli=[self.TopTarget, self.BotTarget, self.LeftTarget, self.RightTarget])
-
+                                  stimuli=[self.TopTarget, self.BotTarget, self.LeftTarget, self.RightTarget])      # For Targets
         self.viewport2 = Viewport(screen=self.screen,
-                                  stimuli=[self.text, self.arrow])
+                                  stimuli=[self.text, self.arrow])                                                  # For cue (and other things)
 
         # Presentations (for controlling timing)
-        self.p = Presentation(go_duration=(3, 'seconds'), viewports=[self.viewport1])
-
-        self.p2 = Presentation(go_duration=(2, 'seconds'), viewports=[self.viewport2])
+        self.p = Presentation(go_duration=(duration_target, 'seconds'), viewports=[self.viewport1])
+        self.p2 = Presentation(go_duration=(duration_cue, 'seconds'), viewports=[self.viewport2])
 
     def on_init(self):
         # Set screen's background color
         self.screen.set(bgcolor=(0., 0., 0.)) # black
+
+        # Set target's properties
+        self.TopTarget.set(size=(HSize, VSize),
+                           color= (1.0, 1.0, 1.0, 1.0),
+                           position=(w/2, h*0.8))
+        self.BotTarget.set(size=(HSize, VSize),
+                           color=(1.0, 1.0, 1.0, 1.0),
+                           position=(w/2, h*0.2))
+        self.LeftTarget.set(size=(VSize, HSize),
+                            color=(1.0, 1.0, 1.0, 1.0),
+                            position=(w*0.2, h/2))
+        self.RightTarget.set(size=(VSize, HSize),
+                             color=(1.0, 1.0, 1.0, 1.0),
+                             position=(w*0.8, h/2))
+
+        # Message
+        # self.text.set(text='Please wait for next trial...',
+        #               color=(1.0, 0.5, 0.5),
+        #               position=(w/2, h*0.8),
+        #               font_size=50,
+        #               anchor='center',
+        #               on=False)
+
+        # Arrows
+        self.arrow.set(texture=Texture('images\left2.bmp'),
+                        position=(w/2, h/2),
+                        anchor='center',
+                        on=False)
 
         # Set control's parameters and corresponding function. Controlling targers
         self.p.add_controller(self.TopTarget, 'on',     FunctionController(during_go_func=self.topFlick))
@@ -93,12 +113,13 @@ class Stimulation:
         # Controlling others
         # self.p2.add_controller(self.text, 'on',         FunctionController(during_go_func=self.appear))
         self.p2.add_controller(self.arrow, 'on',        FunctionController(during_go_func=self.arrow_appear))
+        self.p2.add_controller(self.arrow, 'texture',   FunctionController(during_go_func=self.random_cue))
 
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
 
-        for i in range(0,self.numTrial):
+        for i in range(0,numTrial):
             self.p.go()
             self.p2.go()
 
@@ -127,6 +148,24 @@ class Stimulation:
             return False
         else:
             return True
+
+    def random_cue(self,t):
+        start_time = self.p.last_go_loop_start_time_absolute_sec
+        # if not isinstance(start_time, type(None)):
+        try:    # catch the wrong type of start_time returned earlier
+            i = int(start_time/duration_trial)
+        finally:
+            return Texture('images/left2.bmp')  # Just return something
+
+        cur_target = order_appear[i]        # current target
+        if cur_target == 1:
+            return Texture('images/left2.bmp')
+        elif cur_target == 2:
+            return Texture('images/right2.bmp')
+        elif cur_target == 3:
+            return Texture('images/up2.bmp')
+        elif cur_target == 4:
+            return Texture('images/down2.bmp')
 
     #################################################
     # Writing data methods                          #
